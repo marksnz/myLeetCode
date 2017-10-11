5b88d9c7bd53113f05207d6c4cb0178648d40bf8
marksnz <kaima.gmk@gmail.com>
2017-10-12 00:40:27 +0800

    adjust format : replace tab with whitespace

--- List/AddTwoNumbers.java
+++ List/AddTwoNumbers.java
@@ -8,7 +8,8 @@
 
 public class AddTwoNumbers {
 
-	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) 
+    {
         if (null == l1 || null == l2)
             return null;
         
@@ -19,57 +20,59 @@ public class AddTwoNumbers {
         ListNode newNode = null;
         while (_val != 0)
         {
-        	newNode = new ListNode(_val % 10);
-        	curr.next = newNode;
-        	curr = newNode;
-        	_val /= 10;
+            newNode = new ListNode(_val % 10);
+            curr.next = newNode;
+            curr = newNode;
+            _val /= 10;
         }
         return out;
     }
-	
-	private int getVal(ListNode ln)
+    
+    private int getVal(ListNode ln)
     {
         StringBuilder sval = new StringBuilder(Integer.toString(ln.val));
         while(ln.next != null)
         {
-        	ln = ln.next;
-        	sval.append(Integer.toString(ln.val));
+            ln = ln.next;
+            sval.append(Integer.toString(ln.val));
         }
         return Integer.parseInt(sval.reverse().toString());
     }
-	
-	private void printList(ListNode ln)
-	{
-		System.out.print("(");
-		while(ln.next != null)
-		{
-			System.out.print(ln.val + "->");
-			ln = ln.next;
-		}
-		System.out.print(ln.val + ")");
-	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		AddTwoNumbers atn = new AddTwoNumbers();
-		AddTwoNumbers.ListNode l1 = atn.new ListNode(2);
-		l1.next = atn.new ListNode(4); 
-		l1.next.next = atn.new ListNode(3); 
-		l1.next.next.next = atn.new ListNode(3); 
-		AddTwoNumbers.ListNode l2 = atn.new ListNode(5);
-		l2.next = atn.new ListNode(6); 
-		l2.next.next = atn.new ListNode(4); 
-		
-		AddTwoNumbers.ListNode out = atn.addTwoNumbers(l1, l2);
-		atn.printList(l1);
-		System.out.print(" + ");
-		atn.printList(l2);
-		System.out.print(" = ");
-		atn.printList(out);
-		System.out.println();
-	}
-	
-	class ListNode {
+    
+    private void printList(ListNode ln)
+    {
+        System.out.print("(");
+        while(ln.next != null)
+        {
+            System.out.print(ln.val + "->");
+            ln = ln.next;
+        }
+        System.out.print(ln.val + ")");
+    }
+    
+    public static void main(String[] args) 
+    {
+        // TODO Auto-generated method stub
+        AddTwoNumbers atn = new AddTwoNumbers();
+        AddTwoNumbers.ListNode l1 = atn.new ListNode(2);
+        l1.next = atn.new ListNode(4); 
+        l1.next.next = atn.new ListNode(3); 
+        l1.next.next.next = atn.new ListNode(3); 
+        AddTwoNumbers.ListNode l2 = atn.new ListNode(5);
+        l2.next = atn.new ListNode(6); 
+        l2.next.next = atn.new ListNode(4); 
+        
+        AddTwoNumbers.ListNode out = atn.addTwoNumbers(l1, l2);
+        atn.printList(l1);
+        System.out.print(" + ");
+        atn.printList(l2);
+        System.out.print(" = ");
+        atn.printList(out);
+        System.out.println();
+    }
+    
+    class ListNode 
+    {
         int val;
         ListNode next;
         ListNode(int x) { val = x; }
